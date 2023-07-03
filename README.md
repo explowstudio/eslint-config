4996af021f2d0ecefe1915bc66e48d1b3cb218c0
Gabriel <oi@gabrielteodoro.com>
2023-07-03 19:34:48 -0300

    feat: add eslint-config for nextjs

---
+++ README.md
@@ -0,0 +1,23 @@
+## What is included?
+
+- Standard config base;
+- React hooks plugin;
+- React plugin;
+- Prettier;
+- JSX a11y plugin;
+
+## How to use
+
+1. Install dependency
+
+```bash
+yarn add eslint @explowstudio/eslint-config -D
+```
+
+2. Add `extends` in your file eslint config (Ex: `.eslintrc.json`)
+
+```json
+{
+  "extends": "@explowstudio/eslint-config/next"
+}
+```
---
+++ next.js
@@ -0,0 +1,34 @@
+const { rules } = require('./default')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    'plugin:@next/next/recommended',
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y", "@typescript-eslint"],
+  rules,
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    },
+  },
+};
